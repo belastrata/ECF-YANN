2c3c3172331f4490dd884bf9a6cc87a8cadcbe2f
13010-20-13 <yannfalcao9@gmail.com>
2021-11-05 08:35:02 +0100

    first commit

---
+++ src/main/java/com/example/ECFYANN/EcfyannApplication.java
@@ -0,0 +1,13 @@
+package com.example.ECFYANN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcfyannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcfyannApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ECFYANN/EcfyannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ECFYANN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcfyannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
